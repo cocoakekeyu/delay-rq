44561f04ae788e550646d4129b92baf303c602c3
cocoakekeyu <cocoakekeyu@gmail.com>
2017-12-10 13:36:43 +0800

    Add simple lock

---
+++ delayrq/lock.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+import uuid
+import time
+
+
+class SimpleLock(object):
+    """This a simple redis lock"""
+
+    def __init__(self, connection, lockname, timeout=10, lock_timeout=10):
+        self.connection = connection
+        self.lockname = "lock:" + lockname
+        self.timeout = timeout
+        self.lock_timeout = lock_timeout
+        self.identifier = str(uuid.uuid4())
+
+    def __enter__(self):
+        conn = self.connection
+        lockname = self.lockname
+        lock_timeout = self.lock_timeout
+        identifier = self.identifier
+        end_time = time.time() + self.timeout
+
+        while time.time() < end_time:
+            if conn.setnx(lockname, identifier):
+                conn.expire(lockname, lock_timeout)
+                return self
+            elif not conn.ttl(lockname):
+                conn.expire(lockname, lock_timeout)
+
+            time.sleep(.001)
+
+        return self
+
+    def __exit__(self, type, value, traceback):
+        if type is None:
+            self.connection.delete(self.lockname)
+
+
---
+++ tests/test_lock.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import uuid
+
+from rq.connections import use_connection
+
+from delayrq.lock import SimpleLock
+from tests import RQTestCase
+
+
+class TestLock(RQTestCase):
+
+    def test_lock(self):
+        name = str(uuid.uuid4())
+        conn = self.testconn
+        with SimpleLock(conn, name) as lock:
+            identifier = lock.identifier
+            self.assertEqual(self.testconn.get(lock.lockname).decode('utf-8'),
+                             identifier)
+
+        self.assertTrue(self.testconn.get("lock:"+name) is None)
+
